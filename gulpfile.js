e3f3ed83e35fe89e582acd60324dca97aabf559e
Lakshman <lakshmanvvs@gmail.com>
2016-01-10 16:48:15 -0800

    add nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,24 @@ var gulp = require('gulp');
 
 // plugins
 var connect = require('gulp-connect');
+ var nodemon = require('gulp-nodemon');
 
 
-gulp.task('default', function () {
+gulp.task('default', function(){
+    nodemon({
+        script: './server/app.js'
+        , ext: 'js html'
+        , env: {   'PORT': 5000,
+                   'NODE_ENV': 'development' }
+
+    })
+});
+
+/*gulp.task('default', function () {
     connect.server({
         root: 'tennis-portal-app/',
 
         port: 5002
-    });
+    });*/
 
-    gulp.task('default',['connect']);
-});
+
